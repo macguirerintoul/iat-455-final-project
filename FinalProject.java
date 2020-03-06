5da8c121435324a01491f94280c39a84523e5723
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-06 15:01:27 -0800

    memory leak

--- FinalProject.java
+++ FinalProject.java
@@ -2,6 +2,9 @@ import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.awt.Color;
 import javax.imageio.ImageIO;
 import java.awt.Graphics;
 import javax.swing.JFileChooser;
@@ -10,20 +13,24 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 class FinalProject extends Frame {
 	BufferedImage image;
 
+	static int width = 1280;
+	static int height = 800;
+
 	public FinalProject() {
 		try {
-			// load and display image that the user specifies
-			JFileChooser chooser = new JFileChooser();
-			FileNameExtensionFilter filter = new FileNameExtensionFilter("Images", "jpg", "png");
-			chooser.setFileFilter(filter);
-			int returnVal = chooser.showOpenDialog(null);
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				image = ImageIO.read(chooser.getSelectedFile());
-			}
+			image = ImageIO.read(new File("bird1.jpg"));
+			/*
+			 * // load and display image that the user specifies JFileChooser chooser = new
+			 * JFileChooser(); FileNameExtensionFilter filter = new
+			 * FileNameExtensionFilter("Images", "jpg", "png");
+			 * chooser.setFileFilter(filter); int returnVal = chooser.showOpenDialog(null);
+			 * if (returnVal == JFileChooser.APPROVE_OPTION) { image =
+			 * ImageIO.read(chooser.getSelectedFile()); }
+			 */
 		} catch (Exception e) {
 			System.out.println("Cannot load the provided image");
 		}
-		this.setTitle("Week 3 workshop - Basic image manipulation");
+		this.setTitle("Final Project");
 		this.setVisible(true);
 
 		this.addWindowListener(new WindowAdapter() {
@@ -33,15 +40,32 @@ class FinalProject extends Frame {
 		});
 	}
 
-	public void paint(Graphics g) {
-		int w = 1280;
-		int h = 800;
-		this.setSize(w, h);
+	public void paint() {
+		System.out.println("Calling paint...");
+		ArrayList<Line> lines = generateStrokes(image);
+		for (Line line : lines) {
+			System.out.println(line);
+		}
+		System.out.println("Paint completed.");
+	}
 
-		g.drawImage(image, w / 4, h / 4, w / 2, h / 2, this);
+	private ArrayList<Line> generateStrokes(BufferedImage img) {
+		System.out.println("Generating strokes...");
+		ArrayList<Line> lines = new ArrayList<Line>(25);
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; x < 5; y++) {
+				lines.add(new Line(x, y, x + 5, y + 5, new Color(100, 100, 100)));
+			}
+		}
+		System.out.println("Strokes generated.");
+		return lines;
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Calling main...");
 		FinalProject finalProject = new FinalProject();
+		finalProject.setSize(width, height);
+		System.out.println("hello");
+		finalProject.paint();
 	}
 }
---
+++ Line.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+class Line {
+	final int x1;
+	final int y1;
+	final int x2;
+	final int y2;
+	final Color color;
+
+	public Line(int x1, int y1, int x2, int y2, Color color) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.color = color;
+	}
+}
