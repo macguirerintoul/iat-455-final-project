7a498f8beb9fc59bd7f469e925803cd37b505208
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-06 16:03:04 -0800

    drawing lines

--- FinalProject.java
+++ FinalProject.java
@@ -1,29 +1,24 @@
+import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Random;
-import java.awt.Color;
-
-import java.awt.geom.Line2D;
 import javax.imageio.ImageIO;
-import java.awt.Graphics;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 class FinalProject extends Frame {
 	BufferedImage image;
 	ArrayList<Line2D.Double> lines;
-	Random random;
+
 	static int width = 1280;
 	static int height = 800;
 
 	public FinalProject() {
 		try {
-			image = ImageIO.read(new File("bird1.jpg"));
+			image = ImageIO.read(new File("image.jpg"));
 			/*
 			 * // load and display image that the user specifies JFileChooser chooser = new
 			 * JFileChooser(); FileNameExtensionFilter filter = new
@@ -37,7 +32,6 @@ class FinalProject extends Frame {
 		}
 		this.setTitle("Final Project");
 		this.setVisible(true);
-
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -45,22 +39,31 @@ class FinalProject extends Frame {
 		});
 	}
 
-	public void paint() {
+	public void paint(Graphics g) {
 		System.out.println("Calling paint...");
 		ArrayList<Line> lines = generateStrokes(image);
-		for (Line line : lines) {
-			System.out.println(line);
-		}
+		drawLines(lines, g);
 		System.out.println("Paint completed.");
+	}
+
+	private void drawLines(ArrayList<Line> lines, Graphics g) {
+		for (Line line : lines) {
+			g.setColor(line.color);
+			g.drawLine((int) line.x1, (int) line.y1, (int) line.x2, (int) line.y2);
+		}
 	}
 
 	private ArrayList<Line> generateStrokes(BufferedImage img) {
 		System.out.println("Generating strokes...");
 		ArrayList<Line> lines = new ArrayList<Line>();
-		for (int x = 0; x < img.getWidth(); x++) {
-			for (int y = 0; y < img.getHeight(); y++) {
-				System.out.println("pixel");
-				lines.add(new Line(x, y, x + 5, y + 5, new Color(100, 100, 100)));
+		for (int x = 0; x < width; x = x + 4) {
+			for (int y = 0; y < height; y = y + 4) {
+				try {
+					lines.add(new Line(x, y, x + 4, y + 4, new Color(image.getRGB(x, y))));
+				} catch (Exception e) {
+					// print exception
+				}
+
 			}
 		}
 		System.out.println("Strokes generated.");
@@ -72,6 +75,6 @@ class FinalProject extends Frame {
 		FinalProject finalProject = new FinalProject();
 		finalProject.setSize(width, height);
 		System.out.println("hello");
-		finalProject.paint();
+		finalProject.repaint();
 	}
 }
