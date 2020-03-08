f4cfe1b98266c7abd02beee88f12fcf0f9038529
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-07 17:35:19 -0800

    colour closeness working

--- FinalProject.java
+++ FinalProject.java
@@ -16,13 +16,14 @@ class FinalProject extends Frame {
 	ArrayList<Line2D.Double> lines;
 	int width;
 	int height;
+	boolean isOrientationByGradient = true;
 	double maxStrokeRadius = 12;
 	double minStrokeRadius = 4;
 	double maxStrokeLength = 8;
 	double minStrokeLength = 1;
 	double maxSrokeAngle = 60;
 	double minStrokeAngle = 30;
-	int pixelInterval = 4;
+	int pixelInterval = 16;
 	Utilities ut = new Utilities();
 
 	public FinalProject() {
@@ -67,6 +68,76 @@ class FinalProject extends Frame {
 		}
 	}
 
+	private float getOrientationForPixel(BufferedImage img, int x, int y) {
+		// determine the 'colour distance' of each pixel around the center pixel
+		// if p1 is closest = -45
+		// if p2 is closest = 0
+		// if p3 is closest = 45
+		// ... etc
+		ArrayList<Integer> pixels = new ArrayList<Integer>(8);
+		pixels.add(img.getRGB(x - 1, y - 1));
+		pixels.add(img.getRGB(x, y - 1));
+		pixels.add(img.getRGB(x + 1, y - 1));
+		pixels.add(img.getRGB(x - 1, y));
+		// pixels.add(img.getRGB(x, y));
+		pixels.add(img.getRGB(x + 1, y));
+		pixels.add(img.getRGB(x - 1, y + 1));
+		pixels.add(img.getRGB(x, y + 1));
+		pixels.add(img.getRGB(x + 1, y + 1));
+
+		// set an initial lowest distance
+		double lowestDistance = ut.colorDistance(new Color(img.getRGB(x, y)), new Color(pixels.get(0)));
+		int closestPixel = 0;
+		for (int pixel : pixels) {
+			double distance = ut.colorDistance(new Color(img.getRGB(x, y)), new Color(pixel));
+			// if this pixel's distance is smaller than the current lowest distance...
+			if (distance < lowestDistance) {
+				// keep track of this pixel's location
+				closestPixel = pixels.indexOf(pixel);
+			}
+		}
+		System.out.println("Closest pixel: " + closestPixel);
+		float angle = 0;
+		switch (closestPixel) {
+			case 0:
+				System.out.println("Closest pixel was top left");
+				angle = -45;
+				break;
+			case 1:
+				System.out.println("Closest pixel was top middle");
+				angle = 0;
+				break;
+			case 2:
+				System.out.println("Closest pixel was top right");
+				angle = 45;
+				break;
+			case 3:
+				System.out.println("Closest pixel was left");
+				angle = -90;
+				break;
+			case 4:
+				System.out.println("Closest pixel was right");
+				angle = 90;
+				break;
+			case 5:
+				System.out.println("Closest pixel was bottom left");
+				angle = -135;
+				break;
+			case 6:
+				System.out.println("Closest pixel was bottom");
+				angle = 180;
+				break;
+			case 7:
+				System.out.println("Closest pixel was bottom right");
+				angle = 135;
+				break;
+			default:
+				System.out.println("Colour detection failed.");
+				break;
+		}
+		return angle;
+	}
+
 	private ArrayList<Line> generateStrokes(BufferedImage img) {
 		System.out.println("Generating strokes...");
 		ArrayList<Line> lines = new ArrayList<Line>();
@@ -74,10 +145,16 @@ class FinalProject extends Frame {
 		for (int x = 0; x < width; x = x + pixelInterval) {
 			for (int y = 0; y < height; y = y + pixelInterval) {
 				try {
-					// 0 degree angle is vertical
-					double angleInDegrees = ut.randomDoubleBetween(minStrokeAngle, maxSrokeAngle);
 					double length = ut.randomDoubleBetween(minStrokeLength, maxStrokeLength);
-					float angle = (float) (angleInDegrees * Math.PI / 180);
+					float angle;
+					double angleInDegrees;
+					if (isOrientationByGradient) {
+						angleInDegrees = getOrientationForPixel(img, x, y);
+					} else {
+						// 0 degree angle is vertical
+						angleInDegrees = ut.randomDoubleBetween(minStrokeAngle, maxSrokeAngle);
+					}
+					angle = (float) (angleInDegrees * Math.PI / 180);
 					int endX = (int) (x + length * Math.sin(angle));
 					int endY = (int) (y + length * Math.cos(angle));
 					lines.add(new Line(x, y, endX, endY, new Color(image.getRGB(x, y))));
--- Utilities.java
+++ Utilities.java
@@ -1,9 +1,21 @@
 import java.util.Random;
+import java.awt.Color;
 
 class Utilities {
 	Random random = new Random();
 
 	public double randomDoubleBetween(double min, double max) {
 		return random.nextDouble() * (max - min) + min;
+	}
+
+	double colorDistance(Color c1, Color c2) {
+		// from https://stackoverflow.com/a/6334454/8517613
+		int red1 = c1.getRed();
+		int red2 = c2.getRed();
+		int rmean = (red1 + red2) >> 1;
+		int r = red1 - red2;
+		int g = c1.getGreen() - c2.getGreen();
+		int b = c1.getBlue() - c2.getBlue();
+		return Math.sqrt((((512 + rmean) * r * r) >> 8) + 4 * g * g + (((767 - rmean) * b * b) >> 8));
 	}
 }
