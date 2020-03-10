9e95f858318daa930a8466d1a11d754418909071
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-09 18:08:37 -0700

    2sliders

--- UIControlWindow.java
+++ UIControlWindow.java
@@ -17,66 +17,90 @@ import javax.swing.event.ChangeListener;
  * Demo application panel to display a range slider.
  */
 public class UIControlWindow extends JPanel {
-
-	private JLabel rangeSliderLabel1 = new JLabel();
-	private JLabel rangeSliderValue1 = new JLabel();
-	private JLabel rangeSliderLabel2 = new JLabel();
-	private JLabel rangeSliderValue2 = new JLabel();
 	private RangeSlider radiusSlider = new RangeSlider();
 	private RangeSlider lengthSlider = new RangeSlider();
+
+	private JLabel radiusSliderTitle = new JLabel();
+	private JLabel radiusSliderLabel1 = new JLabel();
+	private JLabel radiusSliderValue1 = new JLabel();
+	private JLabel radiusSliderLabel2 = new JLabel();
+	private JLabel radiusSliderValue2 = new JLabel();
+
+	private JLabel lengthSliderTitle = new JLabel();
+	private JLabel lengthSliderLabel1 = new JLabel();
+	private JLabel lengthSliderValue1 = new JLabel();
+	private JLabel lengthSliderLabel2 = new JLabel();
+	private JLabel lengthSliderValue2 = new JLabel();
 
 	public UIControlWindow() {
 		setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
 		setLayout(new GridBagLayout());
 
 		// RADIUS SLIDER
-		rangeSliderLabel1.setText("Lower value:");
-		rangeSliderLabel2.setText("Upper value:");
-		rangeSliderValue1.setHorizontalAlignment(JLabel.LEFT);
-		rangeSliderValue2.setHorizontalAlignment(JLabel.LEFT);
+		radiusSliderTitle.setText("Stroke radius");
+		radiusSliderLabel1.setText("Lower value:");
+		radiusSliderLabel2.setText("Upper value:");
+		// radiusSliderValue1.setHorizontalAlignment(JLabel.LEFT);
+		// radiusSliderValue2.setHorizontalAlignment(JLabel.LEFT);
 		radiusSlider.setPreferredSize(new Dimension(240, radiusSlider.getPreferredSize().height));
 		radiusSlider.setMinimum(0);
 		radiusSlider.setMaximum(10);
 		radiusSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				RangeSlider slider = (RangeSlider) e.getSource();
-				rangeSliderValue1.setText(String.valueOf(slider.getValue()));
-				rangeSliderValue2.setText(String.valueOf(slider.getUpperValue()));
+				radiusSliderValue1.setText(String.valueOf(slider.getValue()));
+				radiusSliderValue2.setText(String.valueOf(slider.getUpperValue()));
 				System.out.println("RADIUS SLIDER CHANGED");
 			}
 		});
-
-		add(rangeSliderLabel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(radiusSliderTitle, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
-		add(rangeSliderValue1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(radiusSliderLabel1, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(radiusSliderValue1, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 3, 0), 0, 0));
-		add(rangeSliderLabel2, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(radiusSliderLabel2, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
-		add(rangeSliderValue2, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(radiusSliderValue2, new GridBagConstraints(1, 2, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 6, 0), 0, 0));
-		add(radiusSlider, new GridBagConstraints(0, 2, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(radiusSlider, new GridBagConstraints(0, 3, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 
 		// LENGTH SLIDER
-		rangeSliderLabel1.setText("Lower value:");
-		rangeSliderLabel2.setText("Upper value:");
-		rangeSliderValue1.setHorizontalAlignment(JLabel.LEFT);
-		rangeSliderValue2.setHorizontalAlignment(JLabel.LEFT);
+		lengthSliderTitle.setText("Stroke length");
+		lengthSliderLabel1.setText("Lower value:");
+		lengthSliderLabel2.setText("Upper value:");
+		// lengthSliderValue1.setHorizontalAlignment(JLabel.LEFT);
+		// lengthSliderValue2.setHorizontalAlignment(JLabel.LEFT);
 		lengthSlider.setPreferredSize(new Dimension(240, lengthSlider.getPreferredSize().height));
 		lengthSlider.setMinimum(0);
 		lengthSlider.setMaximum(10);
 		lengthSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				RangeSlider slider = (RangeSlider) e.getSource();
-				rangeSliderValue1.setText(String.valueOf(slider.getValue()));
-				rangeSliderValue2.setText(String.valueOf(slider.getUpperValue()));
+				lengthSliderValue1.setText(String.valueOf(slider.getValue()));
+				lengthSliderValue2.setText(String.valueOf(slider.getUpperValue()));
 				System.out.println("LENGTH SLIDER CHANGED");
 			}
 		});
+		JLabel divider = new JLabel();
+		divider.setText("--------------");
 
-		add(lengthSlider, new GridBagConstraints(0, 4, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+		add(divider, new GridBagConstraints(0, 4, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+				new Insets(0, 0, 3, 3), 0, 0));
+
+		add(lengthSliderTitle, new GridBagConstraints(0, 5, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(lengthSliderLabel1, new GridBagConstraints(0, 6, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(lengthSliderValue1, new GridBagConstraints(1, 6, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 0), 0, 0));
+		add(lengthSliderLabel2, new GridBagConstraints(0, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 3, 3), 0, 0));
+		add(lengthSliderValue2, new GridBagConstraints(1, 7, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
+				GridBagConstraints.NONE, new Insets(0, 0, 6, 0), 0, 0));
+		add(lengthSlider, new GridBagConstraints(0, 8, 2, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST,
 				GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-
 	}
 
 	public void display() {
@@ -88,21 +112,22 @@ public class UIControlWindow extends JPanel {
 		lengthSlider.setUpperValue(7);
 
 		// Initialize value display.
-		rangeSliderValue1.setText(String.valueOf(radiusSlider.getValue()));
-		rangeSliderValue2.setText(String.valueOf(radiusSlider.getUpperValue()));
+		radiusSliderValue1.setText(String.valueOf(radiusSlider.getValue()));
+		radiusSliderValue2.setText(String.valueOf(radiusSlider.getUpperValue()));
 
-		// Create window frame.
+		lengthSliderValue1.setText(String.valueOf(lengthSlider.getValue()));
+		lengthSliderValue2.setText(String.valueOf(lengthSlider.getUpperValue()));
+
+		//
+
+		//
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		frame.setTitle("Range Slider Demo");
-
-		// Set window content and validate.
+		frame.setTitle("UI Controls");
 		frame.getContentPane().setLayout(new BorderLayout());
 		frame.getContentPane().add(this, BorderLayout.CENTER);
 		frame.pack();
-
-		// Set window location and display.
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
